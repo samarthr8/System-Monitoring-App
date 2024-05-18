f4b1d83c1a63352b175493925fa835776ac32694
Samarth Rawat <samarthr8@gmail.com>
2024-05-18 14:41:47 +0000

    Pushing the code to github Repository

---
+++ ecr.py
@@ -0,0 +1,11 @@
+import boto3
+from collections.abc import Mapping, MutableMapping
+
+
+ecr_client = boto3.client('ecr')
+
+repository_name = "my_monitoring_app_image"
+response = ecr_client.create_repository(repositoryName=repository_name)
+
+repository_uri = response ['repository']['repositoryUri']
+print(repository_uri)
